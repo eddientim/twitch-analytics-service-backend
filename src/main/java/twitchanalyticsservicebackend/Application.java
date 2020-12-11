514ecf0a9dbfed53407e008d3ff8934a61d54284
eddientim <edward11r@yahoo.co.uk>
2020-12-11 16:56:37 +0000

    adds checkstyle to pipeline

--- src/main/java/twitchanalyticsservicebackend/Application.java
+++ src/main/java/twitchanalyticsservicebackend/Application.java
@@ -1,4 +1,4 @@
-package twitch.analytics.servicebackend;
+package twitchanalyticsservicebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
--- src/test/java/twitchanalyticsservicebackend/analyticsservice/controller/GetUserInfoTest.java
+++ src/test/java/twitchanalyticsservicebackend/analyticsservice/controller/GetUserInfoTest.java
@@ -1,2 +1,19 @@
-package twitchanalyticsservicebackend.analyticsservice.controller.exceptions;public class GetUserInfoTest {
+package twitchanalyticsservicebackend.analyticsservice.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+
+class GetUserInfoTest {
+
+    @Test
+    @DisplayName("GetUserTest_TestEndpoint")
+    void getUserTest() {
+        //Given
+        String userInfo = new GetUserInfo().getHello();
+
+        //Then
+        assertEquals(userInfo,"hello world!!");
+    }
 }
