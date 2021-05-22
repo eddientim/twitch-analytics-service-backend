6c937d71bd2a6c5a383b11893bfae75a9d4ac2f8
eddientim <edward11r@yahoo.co.uk>
2021-05-22 23:00:17 +0100

    removes deprecated WebMvcConfigurerAdapter to WebMvcConfigurer

--- src/main/java/twitchanalyticsservicebackend/analyticsservice/models/TopGamesResponse.java
+++ src/main/java/twitchanalyticsservicebackend/analyticsservice/models/TopGamesResponse.java
@@ -3,6 +3,8 @@ package twitchanalyticsservicebackend.analyticsservice.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.util.UriTemplate;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -11,5 +13,7 @@ public class TopGamesResponse {
 
     private String id;
     private String name;
+    private List<String> pagination;
+    private UriTemplate boxArtUrl;
 
 }
--- src/main/java/twitchanalyticsservicebackend/analyticsservice/service/GamesService.java
+++ src/main/java/twitchanalyticsservicebackend/analyticsservice/service/GamesService.java
@@ -2,22 +2,19 @@ package twitchanalyticsservicebackend.analyticsservice.service;
 
 import org.springframework.stereotype.Service;
 import twitchanalyticsservicebackend.analyticsservice.models.TopGames;
-import twitchanalyticsservicebackend.analyticsservice.models.TopGamesResponse;
 
-import java.util.Optional;
 
 @Service
 public class GamesService {
 
+
     private TopGames topGames;
-    private TopGamesResponse TopGamesResponse;
 
-    public void getTopGames() {
-    }
+    private TopGames checkForTopPlayingGames(final String id, String name) {
+        
+        return topGames;
 
-    private Optional<TopGamesResponse> checkForTopPlayingGames(String id, String name) {
 
-        return Optional.ofNullable(TopGamesResponse);
     }
 
 }
